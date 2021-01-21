d3d475645c770989fe949a479c972f81935ba3c5
bharatjadhav <bharatgjadhav04@gmail.com>
2021-01-21 17:06:46 +0530

    added some basic steps

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
+import { computeHeadingLevel } from "@testing-library/react"
 import React from "react"
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom"
 
-const Part=()=>{
-  <h1>part0</h1>
-}
-const Part1=()=>{
-  <h2>Part1</h2>
-}
-const Part2=()=>{
-  <h1>Part2</h1>
-}
+/* const App=()=>{
+  const course="half stack application development"
+  const part1="fundamentals of react"
+  const ex1=10
+  const part2="using props to pass data"
+  const ex2=7
+  const part3="state of a component"
+  const ex3=14
 
-const App=()=>{
   return(
     <div>
-      <Part/>
-      <Part1/>
-      <Part3/>
-
+      <h1>
+        {course}
+      </h1>
+      <p>
+        {part1} {ex1}
+      </p>
+      <p>
+        {part2} {ex2}
+      </p>
+      <p>
+        {part3} {ex3}
+      </p>
+      <p>Number of exercise {ex1+ex2+ex3}</p>
     </div>
   )
 }
 
+ */
 
+const App=()=>{
+  const course="half stack development"
+  const parts=[
+   {
+    name :"fundamentals of react",
+    exercises : 10
+  },
 
+  {
+    name:"using props to pass data",
+    exercise:7
+  },
 
-
-
-ReactDom.render(<App/>,document.getElementById("root"))
+  {
+    name:"state of a compopnet ",
+    exercise:14
+  }
+  ]
+}
+ReactDOM.render(<App/>, document.getElementById("root"))
