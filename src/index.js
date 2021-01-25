9fc33115b3126ccb30251b818c675fc5adf049c3
bharatjadhav <bharatgjadhav04@gmail.com>
2021-01-25 21:46:42 +0530

    added some code

--- src/index.js
+++ src/index.js
@@ -115,7 +115,7 @@ console.log("rendering .......", counter)
     </div>
   )
 } */
-const Display=({counter})=><div>{counter}</div>
+/* const Display=({counter})=><div>{counter}</div>
 const Button=({handleClick,text})=>(
  <button onClick={handleClick}> 
       {text}
@@ -140,7 +140,51 @@ const App=()=>{
 
     </div>
   )
-}
+} */
+/* const App=()=>{
+  const [left,setLeft]=useState(0)
+  const [right,setRight]=useState(0)
+ return(
+   <div>
+     {left}
+     <button onClick={()=>setLeft(left+1)}>
+       left
+     </button>
+     <button onClick={()=>setRight(right+1)}>
+          right
+     </button>
+     {right}
+   </div>
+ )
+} */
 
+const App=()=>{
+  const [clicks,setClicks]=useState({
+    left:0,right:0
+  })
+  const handleLeftCLick=()=>{
+    const newClicks={
+      left:clicks.left+1,
+      right:clicks.right
+    }
+    setClicks(newClicks)
+  }
+  const handleRightClick=()=>{
+    const newClicks={
+      left:clicks.left,
+      right:clicks.right+1,
+    }
+    setClicks(newClicks)
+  }
+return(
+  <div>
+    {clicks.left}
+    <button onClick={handleLeftCLick}>left</button>
+    <button onClick={handleRightClick}>right</button>
+    {clicks.right}
+  </div>
+)
+
+}
 
 ReactDOM.render(<App/>,document.getElementById("root"))
